6683887d9a87934efab8e63d86228b29fa79eb4e
Milosz Wach <wach.milosz1@gmail.com>
2025-05-15 14:48:37 +0200

    did players

---
+++ src/Player.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+export default function Player({name, symbol}) {
+  const [playerName, setPlayerName] = useState(name)
+  const [isEditing, setIsEditing] = useState(false);
+  
+  function handleClick() {
+    setIsEditing(editing => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  if(isEditing) {
+    editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}></input>;
+  }
+
+  return (
+    <li className='player'>
+      <span>
+          {editablePlayerName}
+          <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleClick}>{isEditing ? 'Save' : 'Edit'}</button>
+    </li>
+  )
+}
